ed7e7038b57fd65a263d74c5e6f50aaa504bec4d
brandonprogram4 <brandonprogram4@gmail.com>
2024-07-22 13:16:47 -0500

    correciones

--- dist/controllers/controllers.user.js
+++ dist/controllers/controllers.user.js
@@ -7,7 +7,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-export class CrudUsers {
+export class UsersCrud {
     constructor(domain) {
         this.domain = domain;
     }
